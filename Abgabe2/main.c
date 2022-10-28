5abe26925ce08e16132b3b4fe9350ca3d77bb423
maximilian <maximilian@maximilian-manjaro-notebook>
2022-10-28 11:25:47 +0200

    Oszimilian:Projekt erstellt

---
+++ Abgabe2/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 28.10.2022
+ *      Author: maximilian
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Abgabe 2\n");
+
+	return EXIT_SUCCESS;
+}
