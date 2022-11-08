688f820b43d3b9041824996de811b9810384aafc
student <student@ubuntu>
2022-11-08 19:40:22 +0100

    Added Aufgabe 5 a

--- Abgabe2/main.c
+++ Abgabe2/main.c
@@ -92,8 +92,96 @@ void rollTheDices() {
 	}
 }
 
+void nickelback() {
+	int muenze_e2 = 0;
+	int muenze_e1 = 0;
+	int muenze_c50 = 0;
+	int muenze_c20 = 0;
+	int muenze_c10 = 0;
+	int muenze_c5 = 0;
+	int muenze_c2 = 0;
+	int muenze_c1 = 0;
+	float eingabe = 0.00;
+
+	printf("Bitte geben Sie einen Betrag bis 200€ mit maximal zwei Nachkommastellen ein. \n");
+	while (eingabe <= 0.00 || eingabe > 200.00) {
+		printf("Ihre Eingabe: ");
+		scanf("%f", &eingabe);
+		if (eingabe <= 0.00 || eingabe > 200.00) {
+			printf("Kein korrekter Betrag eingegeben. Bitte noch einmal wiederhohlen. \n");
+		}
+	}
+
+
+	int eingabe_cent = eingabe * 100;
+	// printf("%i", eingabe_cent);
+
+	while (eingabe_cent > 0) {
+		if (eingabe_cent >= 200 && muenze_e2 < 100) {
+			// 2€ Stück verwenden
+			muenze_e2++;
+			eingabe_cent -= 200;
+		} else if (eingabe_cent >= 100 && muenze_e1 < 100) {
+			// 1€ Stück verwenden
+			muenze_e1++;
+			eingabe_cent -= 100;
+		} else if (eingabe_cent >= 50 && muenze_c50 < 100) {
+			// 50Cent Stück verwenden
+			muenze_c50++;
+			eingabe_cent -= 50;
+		} else if (eingabe_cent >= 20 && muenze_c20 < 100) {
+			// 20Cent Stück verwenden
+			muenze_c20++;
+			eingabe_cent -= 20;
+		} else if (eingabe_cent >= 10 && muenze_c10 < 100) {
+			// 10Cent Stück verwenden
+			muenze_c10++;
+			eingabe_cent -= 10;
+		} else if (eingabe_cent >= 5 && muenze_c5 < 100) {
+			// 5Cent Stück verwenden
+			muenze_c5++;
+			eingabe_cent -= 5;
+		} else if (eingabe_cent >= 2 && muenze_c2 < 100) {
+			// 2Cent Stück verwenden
+			muenze_c2++;
+			eingabe_cent -= 2;
+		} else if (eingabe_cent >= 1 && muenze_c1 < 1) {
+			// 1Cent Stück verwenden
+			muenze_c1++;
+			eingabe_cent -= 1;
+		}
+	}
+	printf("Große Münzen: ");
+	if (muenze_e2 > 0) {
+		printf("%i x 2 Euro, ", muenze_e2);
+	}
+	if (muenze_e1 > 0) {
+		printf("%i x 1 Euro, ", muenze_e1);
+	}
+	if (muenze_c50 > 0) {
+		printf("%i x 50 Cent, ", muenze_c50);
+	}
+	if (muenze_c20 > 0) {
+		printf("%i x 20 Cent, ", muenze_c20);
+	}
+	if (muenze_c10 > 0) {
+		printf("%i x 10 Cent, ", muenze_c10);
+	}
+	if (muenze_c5 > 0) {
+		printf("%i x 5 Cent, ", muenze_c5);
+	}
+	if (muenze_c2 > 0) {
+		printf("%i x 2 Cent, ", muenze_c2);
+	}
+	if (muenze_c1 > 0) {
+		printf("%i x 1 Cent, ", muenze_c1);
+	}
+	printf("\n");
+}
+
 int main(void)
 {
+	/*
 	printf("Abgabe 2\n");
 	printf("\nAufgabe 1\n");
 
@@ -115,9 +203,10 @@ int main(void)
 	// Aufgabe 4
 	printf("\nAufgabe 4\n");
 	rollTheDices();
-
+*/
 	// Aufgabe 5
 	printf("\nAufgabe 5\n");
+	nickelback();
 
 	return EXIT_SUCCESS;
 }
